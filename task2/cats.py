52e686986061207274285e6bcc8ee2d34f8c3d62
Ivan Levytskyi <53571383+IvanLevytskyi@users.noreply.github.com>
2026-05-21 15:36:15 +0300

    Add function to retrieve cat information from file

---
+++ task2/cats.py
@@ -0,0 +1,30 @@
+def get_cats_info(path):
+    cats = []
+
+    try:
+        with open(path, "r", encoding="utf-8") as file:
+            for line in file:
+                cat_id, name, age = line.strip().split(",")
+
+                cat_info = {
+                    "id": cat_id,
+                    "name": name,
+                    "age": age
+                }
+
+                cats.append(cat_info)
+
+        return cats
+
+    except FileNotFoundError:
+        print("File not found.")
+        return []
+
+    except ValueError:
+        print("Invalid file data format.")
+        return []
+
+
+if __name__ == "__main__":
+    cats_info = get_cats_info("cats_file.txt")
+    print(cats_info)
